5361c0e1e7d2e8d3bf1099718a82f8282d2004ea
tecshuttle <tecshuttle@gmail.com>
2016-05-11 12:06:59 +0800

    [微信] 团购页、分类页。

--- m/js/services.js
+++ m/js/services.js
@@ -3,30 +3,15 @@ angular.module('starter.services', [])
     .factory('Chats', function ($http, $rootScope) {
         // Some fake testing data
         var chats = [{
-            id: 0,
-            name: 'Ben Sparrow',
-            lastText: 'You on your way?',
-            face: 'img/ben.png'
+            id: 0, name: 'Ben Sparrow', lastText: 'You on your way?', face: 'img/ben.png'
         }, {
-            id: 1,
-            name: 'Max Lynx',
-            lastText: 'Hey, it\'s me',
-            face: 'img/max.png'
+            id: 1, name: 'Max Lynx', lastText: 'Hey, it\'s me', face: 'img/max.png'
         }, {
-            id: 2,
-            name: 'Adam Bradleyson',
-            lastText: 'I should buy a boat',
-            face: 'img/adam.jpg'
+            id: 2, name: 'Adam Bradleyson', lastText: 'I should buy a boat', face: 'img/adam.jpg'
         }, {
-            id: 3,
-            name: 'Perry Governor',
-            lastText: 'Look at my mukluks!',
-            face: 'img/perry.png'
+            id: 3, name: 'Perry Governor', lastText: 'Look at my mukluks!', face: 'img/perry.png'
         }, {
-            id: 4,
-            name: 'Mike Harrington',
-            lastText: 'This is wicked good ice cream.',
-            face: 'img/mike.png'
+            id: 4, name: 'Mike Harrington', lastText: 'This is wicked good ice cream.', face: 'img/mike.png'
         }];
 
         return {
@@ -47,6 +32,28 @@ angular.module('starter.services', [])
                     }
                 }
                 return null;
+            }
+        };
+    })
+    .factory('Tuan', function ($http, $rootScope) {
+        return {
+            all: function ($scope) {
+                $http.post('/admin/getList?module=tuan&start=0&limit=9999', {
+                    //module: 'tuan'
+                }).success(function (result, status, headers, config) {
+                    $scope.tuan = result.data;
+                });
+            }
+        };
+    })
+    .factory('Cat', function ($http, $rootScope) {
+        return {
+            all: function ($scope) {
+                $http.post('/admin/getList?module=cat&start=0&limit=9999', {
+                    //module: 'tuan'
+                }).success(function (result, status, headers, config) {
+                    $scope.cat = result.data;
+                });
             }
         };
     });
